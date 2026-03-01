1ecd0486c71660f1e4763de4ee8cd15b04fee90a
mailtokgk <your-email@example.com>
2026-03-01 23:06:33 +0530

    UC3 String.join implementation

---
+++ uc3.java
@@ -0,0 +1,16 @@
+public class uc3 {
+    public static void main(String[] args) {
+
+        String line1 = String.join("   ", "OOOO", "OOOO", "PPPP", "PPPP");
+        String line2 = String.join("   ", "O  O", "O  O", "P  P", "P  P");
+        String line3 = String.join("   ", "O  O", "O  O", "PPPP", "PPPP");
+        String line4 = String.join("   ", "O  O", "O  O", "P", "P");
+        String line5 = String.join("   ", "OOOO", "OOOO", "P", "P");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
