8678ed932d17c72912278580a697547d610f16e5
iniya-kannan <sudhainiya2006@gmail.com>
2026-03-01 22:35:57 +0530

    Update uc1.java

---
+++ uc1.java
@@ -0,0 +1,7 @@
+class uc1 {
+    public static void main(String[] args) {
+        System.out.println("======== OOPS BANNER ========");
+        System.out.println("   Welcome to OOPS Program   ");
+        System.out.println("=============================");
+    }
+}
